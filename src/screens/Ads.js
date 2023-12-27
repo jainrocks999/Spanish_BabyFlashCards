c7c9deaf0024905732f6b682a0d7592ce123e4ec
RAJU BARDE <125546485+rajuforbear@users.noreply.github.com>
2023-12-27 15:06:33 +0530

    changes

--- src/screens/Ads.js
+++ src/screens/Ads.js
@@ -1,9 +1,7 @@
 import {TestIds} from 'react-native-google-mobile-ads';
 export default {
-  interstitial: TestIds.INTERSTITIAL,
-  // 'ca-app-pub-3339897183017333/8255680786',
-  BANNER: TestIds.BANNER,
-  // 'ca-app-pub-3339897183017333/1430417985',
+  interstitial: 'ca-app-pub-3339897183017333/8255680786',
+  BANNER: 'ca-app-pub-3339897183017333/1430417985',
 };
 
 //intn //
